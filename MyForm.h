cab1068c0650727b9b39bc9b8f734c4b5e7c1716
azamatshsh123@gmail.com <azamatshsh123@gmail.com>
2023-04-18 21:49:36 +0300

    pred

--- MyForm.h
+++ MyForm.h
@@ -47,6 +47,9 @@ namespace oop6laba {
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Label^ label5;
 
 	private:
 		/// <summary>
@@ -73,110 +76,127 @@ namespace oop6laba {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(12, 375);
+			this->button1->BackColor = System::Drawing::Color::White;
+			this->button1->Location = System::Drawing::Point(9, 53);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(120, 44);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"Квадрат";
-			this->button1->UseVisualStyleBackColor = true;
+			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(138, 375);
+			this->button2->BackColor = System::Drawing::Color::White;
+			this->button2->Location = System::Drawing::Point(153, 53);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(120, 44);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"Круг";
-			this->button2->UseVisualStyleBackColor = true;
+			this->button2->UseVisualStyleBackColor = false;
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(264, 375);
+			this->button3->BackColor = System::Drawing::Color::White;
+			this->button3->Location = System::Drawing::Point(297, 53);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(120, 44);
 			this->button3->TabIndex = 2;
 			this->button3->Text = L"Треугольник";
-			this->button3->UseVisualStyleBackColor = true;
+			this->button3->UseVisualStyleBackColor = false;
 			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
 			// 
 			// radioButton1
 			// 
 			this->radioButton1->AutoSize = true;
-			this->radioButton1->Location = System::Drawing::Point(12, 348);
+			this->radioButton1->Location = System::Drawing::Point(9, 26);
 			this->radioButton1->Name = L"radioButton1";
 			this->radioButton1->Size = System::Drawing::Size(88, 21);
 			this->radioButton1->TabIndex = 3;
 			this->radioButton1->TabStop = true;
 			this->radioButton1->Text = L"Зеленый";
 			this->radioButton1->UseVisualStyleBackColor = true;
+			this->radioButton1->Visible = false;
 			this->radioButton1->CheckedChanged += gcnew System::EventHandler(this, &MyForm::radioButton1_CheckedChanged);
+			this->radioButton1->Click += gcnew System::EventHandler(this, &MyForm::radioButton1_Click);
 			// 
 			// radioButton2
 			// 
 			this->radioButton2->AutoSize = true;
-			this->radioButton2->Location = System::Drawing::Point(106, 348);
+			this->radioButton2->Location = System::Drawing::Point(116, 26);
 			this->radioButton2->Name = L"radioButton2";
 			this->radioButton2->Size = System::Drawing::Size(87, 21);
 			this->radioButton2->TabIndex = 4;
 			this->radioButton2->TabStop = true;
 			this->radioButton2->Text = L"Красный";
 			this->radioButton2->UseVisualStyleBackColor = true;
+			this->radioButton2->Visible = false;
 			this->radioButton2->CheckedChanged += gcnew System::EventHandler(this, &MyForm::radioButton2_CheckedChanged);
+			this->radioButton2->Click += gcnew System::EventHandler(this, &MyForm::radioButton2_Click);
 			// 
 			// radioButton3
 			// 
 			this->radioButton3->AutoSize = true;
-			this->radioButton3->Location = System::Drawing::Point(199, 348);
+			this->radioButton3->Location = System::Drawing::Point(223, 26);
 			this->radioButton3->Name = L"radioButton3";
 			this->radioButton3->Size = System::Drawing::Size(70, 21);
 			this->radioButton3->TabIndex = 5;
 			this->radioButton3->TabStop = true;
 			this->radioButton3->Text = L"Синий";
 			this->radioButton3->UseVisualStyleBackColor = true;
+			this->radioButton3->Visible = false;
 			this->radioButton3->CheckedChanged += gcnew System::EventHandler(this, &MyForm::radioButton3_CheckedChanged);
+			this->radioButton3->Click += gcnew System::EventHandler(this, &MyForm::radioButton3_Click);
 			// 
 			// radioButton4
 			// 
 			this->radioButton4->AutoSize = true;
-			this->radioButton4->Location = System::Drawing::Point(275, 348);
+			this->radioButton4->Location = System::Drawing::Point(307, 26);
 			this->radioButton4->Name = L"radioButton4";
 			this->radioButton4->Size = System::Drawing::Size(110, 21);
 			this->radioButton4->TabIndex = 6;
 			this->radioButton4->TabStop = true;
 			this->radioButton4->Text = L"Без заливки";
 			this->radioButton4->UseVisualStyleBackColor = true;
+			this->radioButton4->Visible = false;
 			this->radioButton4->CheckedChanged += gcnew System::EventHandler(this, &MyForm::radioButton4_CheckedChanged);
+			this->radioButton4->Click += gcnew System::EventHandler(this, &MyForm::radioButton4_Click);
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(518, 375);
+			this->button4->BackColor = System::Drawing::Color::White;
+			this->button4->Location = System::Drawing::Point(561, 53);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(120, 44);
 			this->button4->TabIndex = 7;
 			this->button4->Text = L"Очистить форму";
-			this->button4->UseVisualStyleBackColor = true;
+			this->button4->UseVisualStyleBackColor = false;
 			// 
 			// button5
 			// 
-			this->button5->Location = System::Drawing::Point(392, 375);
+			this->button5->BackColor = System::Drawing::Color::White;
+			this->button5->Location = System::Drawing::Point(435, 53);
 			this->button5->Name = L"button5";
 			this->button5->Size = System::Drawing::Size(120, 44);
 			this->button5->TabIndex = 8;
-			this->button5->Text = L"Включить ColorPanel";
-			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Text = L"Открыть ColorPanel";
+			this->button5->UseVisualStyleBackColor = false;
 			this->button5->Click += gcnew System::EventHandler(this, &MyForm::button5_Click);
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
 			this->label1->ForeColor = System::Drawing::Color::Red;
-			this->label1->Location = System::Drawing::Point(427, 348);
+			this->label1->Location = System::Drawing::Point(463, 26);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(181, 17);
 			this->label1->TabIndex = 9;
@@ -186,11 +206,12 @@ namespace oop6laba {
 			// 
 			this->label2->AutoSize = true;
 			this->label2->ForeColor = System::Drawing::Color::Green;
-			this->label2->Location = System::Drawing::Point(44, 328);
+			this->label2->Location = System::Drawing::Point(59, 6);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(307, 17);
 			this->label2->TabIndex = 10;
 			this->label2->Text = L"ColorPanel, редактирование цвета включено!";
+			this->label2->Visible = false;
 			// 
 			// label3
 			// 
@@ -201,35 +222,73 @@ namespace oop6laba {
 			this->label3->Size = System::Drawing::Size(0, 17);
 			this->label3->TabIndex = 11;
 			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->ForeColor = System::Drawing::Color::Green;
+			this->label4->Location = System::Drawing::Point(473, 6);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(161, 17);
+			this->label4->TabIndex = 12;
+			this->label4->Text = L"Клаивша CTRL зажата!";
+			this->label4->Visible = false;
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::NavajoWhite;
+			this->panel1->Controls->Add(this->label5);
+			this->panel1->Controls->Add(this->label4);
+			this->panel1->Controls->Add(this->label2);
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Controls->Add(this->button5);
+			this->panel1->Controls->Add(this->button4);
+			this->panel1->Controls->Add(this->radioButton4);
+			this->panel1->Controls->Add(this->radioButton3);
+			this->panel1->Controls->Add(this->radioButton2);
+			this->panel1->Controls->Add(this->radioButton1);
+			this->panel1->Controls->Add(this->button3);
+			this->panel1->Controls->Add(this->button2);
+			this->panel1->Controls->Add(this->button1);
+			this->panel1->Location = System::Drawing::Point(0, 340);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(696, 109);
+			this->panel1->TabIndex = 13;
+			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::panel1_Paint);
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->ForeColor = System::Drawing::Color::Red;
+			this->label5->Location = System::Drawing::Point(136, 26);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(157, 17);
+			this->label5->TabIndex = 14;
+			this->label5->Text = L"ColorPanel не активна!";
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(650, 431);
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->ClientSize = System::Drawing::Size(696, 449);
+			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
-			this->Controls->Add(this->button5);
-			this->Controls->Add(this->button4);
-			this->Controls->Add(this->radioButton4);
-			this->Controls->Add(this->radioButton3);
-			this->Controls->Add(this->radioButton2);
-			this->Controls->Add(this->radioButton1);
-			this->Controls->Add(this->button3);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::MyForm_Paint);
 			this->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseClick);
 			this->MouseDoubleClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseDoubleClick);
+			this->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::MyForm_MouseDown);
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		button1->BackColor = Color::Red;
+		button1->BackColor = Color::CornflowerBlue;
 		button2->BackColor = Color::White;
 		button3->BackColor = Color::White;
 		mvc.b1Set(1);
@@ -237,7 +296,7 @@ namespace oop6laba {
 		mvc.b3Set(0);
 	}
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-		button2->BackColor = Color::Red;
+		button2->BackColor = Color::CornflowerBlue;
 		button1->BackColor = Color::White;
 		button3->BackColor = Color::White;
 		mvc.b1Set(0);
@@ -245,7 +304,7 @@ namespace oop6laba {
 		mvc.b3Set(0);
 	}
 	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
-		button3->BackColor = Color::Red;
+		button3->BackColor = Color::CornflowerBlue;
 		button1->BackColor = Color::White;
 		button2->BackColor = Color::White;
 		mvc.b1Set(0);
@@ -259,7 +318,16 @@ namespace oop6laba {
 	private: System::Void radioButton3_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void MyForm_MouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
-		mvc.pushObject(0,e);
+		switch (e->Button) {
+		case System::Windows::Forms::MouseButtons::Left:
+			mvc.makeunCheck();
+			mvc.pushObject(mvc.getSize(), e);
+			break;
+		case System::Windows::Forms::MouseButtons::Right:
+			mvc.makeunCheck();
+			mvc.makeCheck(e);
+			break;
+		}
 		Invalidate();
 	}
 	private: System::Void MyForm_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
@@ -270,7 +338,66 @@ namespace oop6laba {
 	}
 	private: System::Void radioButton4_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
-private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
+		switch (radioButton1->Visible)
+		{
+		case true:
+			radioButton1->Visible = false;
+			radioButton2->Visible = false;
+			radioButton3->Visible = false;
+			radioButton4->Visible = false;
+			radioButton1->Checked = false;
+			radioButton2->Checked = false;
+			radioButton3->Checked = false;
+			radioButton4->Checked = false;
+			label2->Visible = false;
+			button5->Text = "Открыть ColorPanel";
+			label5->Visible = true;
+			mvc.colorSet("Empty");
+			break;
+		case false:
+			radioButton1->Visible = true;
+			radioButton2->Visible = true;
+			radioButton3->Visible = true;
+			radioButton4->Visible = true;
+			label2->Visible = true;
+			label5->Visible = false;
+			button5->Text = "Закрыть ColorPanel";
+			break;
+		}
+	}
+private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+}
+private: System::Void MyForm_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+	
+}
+private: System::Void radioButton1_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (radioButton1->Checked) {
+		mvc.colorSet("Green");
+		mvc.createColor();
+	}
+	Invalidate();
+}
+private: System::Void radioButton2_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (radioButton2->Checked) {
+		mvc.colorSet("Red");
+		mvc.createColor();
+	}
+	Invalidate();
+}
+private: System::Void radioButton3_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (radioButton3->Checked) {
+		mvc.colorSet("Blue");
+		mvc.createColor();
+	}
+	Invalidate();
+}
+private: System::Void radioButton4_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (radioButton4->Checked) {
+		mvc.colorSet("Empty");
+		mvc.createColor();
+	}
+	Invalidate();
 }
 };
 }
--- figure.h
+++ figure.h
@@ -1,19 +1,27 @@
 #pragma once
 #include"container.h"
-class FigureInterface {
+#include<math.h>
+#include<gcroot.h>
+ref class FigureInterface {
 public:
-	std::string color="Black";
+	std::string color="Empty";
 	float pointx, pointy,h,w,rad;
-	virtual void setColor(std::string color) {
+	bool checked = true;
+	void setColor(std::string color) {
+		this->color = color;
 	}
-	virtual void changePos(System::EventArgs^ e) {
+	virtual void changePos(System::Windows::Forms::MouseEventArgs^ e) {
 	}
-	virtual void checkPos(System::EventArgs^ e) {
+	virtual bool checkPos(System::Windows::Forms::MouseEventArgs^ e) {
+		return false;
 	}
 	virtual void drawFigure(System::Windows::Forms::PaintEventArgs^ e) {
 	}
+	void setCheck(bool check) {
+		checked = check;
+	}
 };
-class Rectang : public FigureInterface {
+ref class Rectang : public FigureInterface {
 public:
 	float h, w;
 	Rectang(float x,float y,float h,float w) {
@@ -22,58 +30,86 @@ public:
 		pointx = x-w/2;
 		pointy = y-h/2;
 	}
-	void setColor(std::string color) override {
-		this->color = color;
-	}
 	void drawFigure(System::Windows::Forms::PaintEventArgs^ e) override {
-		if (color == "Black")
-			e->Graphics->DrawRectangle(System::Drawing::Pens::Black, pointx, pointy, w, h);
-		if (color == "Red")
-			e->Graphics->DrawRectangle(System::Drawing::Pens::Red, pointx, pointy, w, h);
+		if (color =="Red")
+			e->Graphics->FillRectangle(System::Drawing::Brushes::Red, pointx, pointy, w, h);
 		if (color == "Blue")
-			e->Graphics->DrawRectangle(System::Drawing::Pens::Blue, pointx, pointy, w, h);
+			e->Graphics->FillRectangle(System::Drawing::Brushes::Blue, pointx, pointy, w, h);
 		if (color == "Green")
-			e->Graphics->DrawRectangle(System::Drawing::Pens::Green, pointx, pointy, w, h);
+			e->Graphics->FillRectangle(System::Drawing::Brushes::Green, pointx, pointy, w, h);
+		e->Graphics->DrawRectangle(System::Drawing::Pens::Black, pointx, pointy, w, h);
+		if (checked)
+			e->Graphics->DrawRectangle(System::Drawing::Pens::Purple, pointx, pointy, w, h);
+	}
+	bool checkPos(System::Windows::Forms::MouseEventArgs^ e) override {
+		if (e->X >= pointx && e->X <= pointx + w && e->Y >= pointy && e->Y <= pointy + h)
+			return true;
+		return false;
 	}
 };
-class Triang : public FigureInterface {
+ref class Triang : public FigureInterface {
 public:
+	array <System::Drawing::Point>^ points;
 	Triang(float x, float y) {
 		pointx = x;
 		pointy = y;
 		h = 675;
 		w = 30;
 	}
-	void setColor(std::string color) override {
-		this->color = color;
+	void drawColor(System::Windows::Forms::PaintEventArgs^ e) {
+		float temph = h,tempw=w;
+		while (tempw) {
+			tempw--;
+			temph--;
+			if (color == "Red") {
+				e->Graphics->DrawLine(System::Drawing::Pens::Red, pointx, pointy - sqrt(temph), pointx - tempw, pointy + sqrt(temph));
+				e->Graphics->DrawLine(System::Drawing::Pens::Red, pointx - tempw, pointy + sqrt(temph), pointx + tempw, pointy + sqrt(temph));
+				e->Graphics->DrawLine(System::Drawing::Pens::Red, pointx, pointy - sqrt(temph), pointx + tempw, pointy + sqrt(temph));
+			}
+		}
 	}
 	void drawFigure(System::Windows::Forms::PaintEventArgs^ e) override {
-		
-		if (color == "Black") {
-			e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx, pointy-sqrt(h), pointx-w, pointy+sqrt(h));
-			e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx-w, pointy + sqrt(h), pointx+w, pointy + sqrt(h));
-			e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx, pointy - sqrt(h), pointx + w, pointy + sqrt(h));
+		drawColor(e);
+		e->Graphics->FillPolygon(System::Drawing::Pens::Cyan, );
+		e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx, pointy - sqrt(h), pointx - w, pointy + sqrt(h));
+		e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx - w, pointy + sqrt(h), pointx + w, pointy + sqrt(h));
+		e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx, pointy - sqrt(h), pointx + w, pointy + sqrt(h));
+		if (checked) {
+			e->Graphics->DrawLine(System::Drawing::Pens::Purple, pointx, pointy - sqrt(h), pointx - w, pointy + sqrt(h));
+			e->Graphics->DrawLine(System::Drawing::Pens::Purple, pointx - w, pointy + sqrt(h), pointx + w, pointy + sqrt(h));
+			e->Graphics->DrawLine(System::Drawing::Pens::Purple, pointx, pointy - sqrt(h), pointx + w, pointy + sqrt(h));
 		}
-		if (color == "Red") {
-			e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx, pointy - sqrt(h), pointx - w, pointy + sqrt(h));
-			e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx - w, pointy + sqrt(h), pointx + w, pointy + sqrt(h));
-			e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx, pointy - sqrt(h), pointx + w, pointy + sqrt(h));
-		}
-		if (color == "Blue") {
-			e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx, pointy - sqrt(h), pointx - w, pointy + sqrt(h));
-			e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx - w, pointy + sqrt(h), pointx + w, pointy + sqrt(h));
-			e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx, pointy - sqrt(h), pointx + w, pointy + sqrt(h));
-		}
-		if (color == "Green") {
-			e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx, pointy - sqrt(h), pointx - w, pointy + sqrt(h));
-			e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx - w, pointy + sqrt(h), pointx + w, pointy + sqrt(h));
-			e->Graphics->DrawLine(System::Drawing::Pens::Black, pointx, pointy - sqrt(h), pointx + w, pointy + sqrt(h));
-		}
-			
+	}
+	float area(int x1, int y1, int x2, int y2, int x3, int y3)
+	{
+		return abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0);
+	}
+	bool isInside(int x1, int y1, int x2, int y2, int x3, int y3, int x, int y)
+	{
+		/* Calculate area of triangle ABC */
+		float A = area(x1, y1, x2, y2, x3, y3);
+
+		/* Calculate area of triangle PBC */
+		float A1 = area(x, y, x2, y2, x3, y3);
+
+		/* Calculate area of triangle PAC */
+		float A2 = area(x1, y1, x, y, x3, y3);
+
+		/* Calculate area of triangle PAB */
+		float A3 = area(x1, y1, x2, y2, x, y);
+
+		/* Check if sum of A1, A2 and A3 is same as A */
+		return (A == A1 + A2 + A3);
+	}
+	bool checkPos(System::Windows::Forms::MouseEventArgs^ e) override {
+		if (isInside(pointx-w,pointy+sqrt(h),pointx,pointy-sqrt(h),pointx+w,pointy+sqrt(h),e->X,e->Y))
+			return true;
+		return false;
 	}
 };
-class Circle : public FigureInterface {
+ref class Circle : public FigureInterface {
 public:
+
 	Circle() {
 
 	}
@@ -81,30 +117,34 @@ public:
 		pointx = x;
 		pointy = y;
 		rad = radp;
-		color = "Black";
-	}
-	void setColor(std::string color) override {
-		this->color = color;
 	}
 	void drawFigure(System::Windows::Forms::PaintEventArgs^ e) override {
-		if (color == "Black")
-			e->Graphics->DrawEllipse(System::Drawing::Pens::Black, pointx - rad, pointy - rad, rad * 2, rad * 2);
 		if (color == "Red")
-			e->Graphics->DrawEllipse(System::Drawing::Pens::Red, pointx - rad, pointy - rad, rad * 2, rad * 2);
-		if (color == "Blue")
-			e->Graphics->DrawEllipse(System::Drawing::Pens::Blue, pointx - rad, pointy - rad, rad * 2, rad * 2);
+			e->Graphics->FillEllipse(System::Drawing::Brushes::Red, pointx - rad, pointy - rad, rad * 2, rad * 2);
 		if (color == "Green")
-			e->Graphics->DrawEllipse(System::Drawing::Pens::Green, pointx - rad, pointy - rad, rad * 2, rad * 2);
+			e->Graphics->FillEllipse(System::Drawing::Brushes::Green, pointx - rad, pointy - rad, rad * 2, rad * 2);
+		if (color == "Blue")
+			e->Graphics->FillEllipse(System::Drawing::Brushes::Blue, pointx - rad, pointy - rad, rad * 2, rad * 2);
+		e->Graphics->DrawEllipse(System::Drawing::Pens::Black, pointx - rad, pointy - rad, rad * 2, rad * 2);
+		if (checked)
+			e->Graphics->DrawEllipse(System::Drawing::Pens::Purple, pointx - rad, pointy - rad, rad * 2, rad * 2);
+	}
+	bool checkPos(System::Windows::Forms::MouseEventArgs^ e) override {
+		if (System::Math::Pow(e->X - pointx, 2) + System::Math::Pow(e->Y - pointy, 2) <= System::Math::Pow(rad, 2))
+			return true;
+		return false;
 	}
 };
 class MVC {
 private:
 	int b1, b2, b3;
-	Storage <FigureInterface*> figures;
+	Storage <FigureInterface^> figures;
+	std::string color;
 public:
 	void b1Set(int a) { b1 = a; }
 	void b2Set(int a) { b2 = a; }
 	void b3Set(int a) { b3 = a; }
+	void colorSet(std::string color) { this->color = color; }
 	int b1Get() { return b1; }
 	int b2Get() { return b2; }
 	int b3Get() { return b3; }
@@ -117,10 +157,32 @@ public:
 			figures.pushObject(i, new Triang(e->X, e->Y));
 	}
 	void drawFigures(System::Windows::Forms::PaintEventArgs^ e) { 
-		for (int i = 0; i < figures.getSize(); i++)
+		for (int i = 0; i<figures.getSize(); i++)
 			figures.getObject(i)->drawFigure(e);
 	}
 	int getSize() { return figures.getSize(); }
+	void changePosition(System::Windows::Forms::MouseEventArgs^ e) {
+
+	}
+	void makeunCheck() {
+		for (int i = figures.getSize() - 1; i >= 0; --i)
+			figures.getObject(i)->setCheck(false);
+			
+	}
+	void makeCheck(System::Windows::Forms::MouseEventArgs^ e) {
+		for (int i = figures.getSize() - 1; i >= 0; --i)
+			if (figures.getObject(i)->checkPos(e)) {
+				figures.getObject(i)->setCheck(!figures.getObject(i)->checked);
+				return;
+			}
+	}
+	void createColor() {
+		for (int i = figures.getSize()-1; i>=0; --i) 
+			if (figures.getObject(i)->checked) { 
+				figures.getObject(i)->color = this->color;
+			}
+	}
+	
 };
 
 
